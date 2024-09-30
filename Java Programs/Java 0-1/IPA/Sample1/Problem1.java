a1f26f37763d6d3bd9e1e4d8a4af1c4984be6524
Khush786Mohammad <khush786moh@gmail.com>
2024-09-30 21:00:03 +0530

    IPA

---
+++ Java Programs/Java 0-1/IPA/Sample1/Problem1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Problem1{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+
+        int temp = number;
+        int myAns = 0;
+        while(temp != 0){
+            int rem = temp%10;
+            temp = temp/10;
+            myAns = myAns*10 + rem;
+        }
+
+        System.out.println(myAns);
+        sc.close();
+    }
+}
